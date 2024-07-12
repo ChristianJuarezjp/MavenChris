e37848c1b141de65e8e0889c4a0ce8124808adcf
ChristianJuarezjp <chrisjuarezale@gmail.com>
2024-07-11 20:04:04 -0600

    Tarea Maven Christian

---
+++ src/main/java/mavenchris/example/mavenchris/MavenchrisApplication.java
@@ -0,0 +1,13 @@
+package mavenchris.example.mavenchris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenchrisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenchrisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mavenchris/example/mavenchris/MavenchrisApplicationTests.java
@@ -0,0 +1,13 @@
+package mavenchris.example.mavenchris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenchrisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
